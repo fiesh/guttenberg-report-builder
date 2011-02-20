1ae983d283b675c9acfe296c15a022b9ed33f2de
fiesh <fiesh@fiesh.homeip.net>
2011-02-20 14:43:29 +0100

    compression, 8 bit depth

--- create_blurry.sh
+++ create_blurry.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 mkdir -p web/images && 
-for ((x=1; x<=475; x++)) { convert -gaussian-blur 10x10 images/`printf %03d $x`.png web/images/`printf %03d $x`_blur.png }
+for ((x=1; x<=475; x++)) { convert -gaussian-blur 10x10 images/`printf %03d $x`.png -quality 100 -define png:bit-depth=8 web/images/`printf %03d $x`_blur.png }
