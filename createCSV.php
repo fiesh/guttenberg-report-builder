0813120bfacf265238a596af775b5be24e58b505
fiesh <fiesh@fiesh.homeip.net>
2011-03-01 10:17:24 +0100

    nur fragment mit kategorie in whitelist beruecksichtigen

--- createCSV.php
+++ createCSV.php
@@ -1,20 +1,24 @@
 <?php
 
+$whitelist = array('KomplettPlagiat', 'Verschleierung', 'HalbsatzFlickerei', 'ShakeAndPaste', 'ÜbersetzungsPlagiat', 'StrukturPlagiat', 'BauernOpfer', 'VerschärftesBauernOpfer');
+
 require_once('FragmentLoader.php');
 
 $ret = '';
 $fragments = FragmentLoader::getFragments();
 
 foreach($fragments as $f) {
-	$first = true;
-	for($i = 1; $i < 12; $i++) { 
-		if(!$first)
-			$ret .= ',';
-		else
-			$first = false;
-		$ret .= $f[$i];
+	if(in_array($f[7], $whitelist)) {
+		$first = true;
+		for($i = 1; $i < 12; $i++) { 
+			if(!$first)
+				$ret .= ',';
+			else
+				$first = false;
+			$ret .= $f[$i];
+		}
+		$ret .= "\n";
 	}
-	$ret .= "\n";
 }
 
 echo 'Seite,Zeilen,TextDissertation,SeiteFundstelle,ZeilenFundstelle,TextFundstelle,Kategorie,ImLiteraturverzeichnis,Quelle,URL,Anmerkung'."\n";
