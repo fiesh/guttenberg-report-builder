31bf1f5bc792b767153658a92a59c597ae2c0453
Kahrl <kahrl@gmx.net>
2011-06-05 11:15:33 +0200

    Fixed CSV generator

--- createCSV.php
+++ createCSV.php
@@ -4,18 +4,31 @@ $whitelist = array('KomplettPlagiat', 'Verschleierung', 'HalbsatzFlickerei', 'Sh
 
 require_once('FragmentLoader.php');
 
+function quoteForCSV($s) {
+	$s = htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+	$s = str_replace(array("\n", "\r"), array('&#10;', '&#13;'), $s);
+	if (strpos($s, ',') !== false)
+		$s = '"'.$s.'"';
+	return $s;
+}
+
+function replaceCategoryLinks($s) {
+	return preg_replace('/\[\[:Kategorie:([^\]\|]+)(\|[^\]]*)?\]\]/', 'http://de.guttenplag.wikia.com/wiki/Kategorie:$1', $s);
+}
+
 $ret = '';
 $fragments = FragmentLoader::getFragments();
 
 foreach($fragments as $f) {
 	if(in_array($f[7], $whitelist)) {
+		$f[10] = replaceCategoryLinks($f[10]);
 		$first = true;
 		for($i = 1; $i < 12; $i++) { 
 			if(!$first)
 				$ret .= ',';
 			else
 				$first = false;
-			$ret .= $f[$i];
+			$ret .= quoteForCSV($f[$i]);
 		}
 		$ret .= "\n";
 	}
